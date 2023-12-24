077a5165225cd69d8337b3ba1fcb8db3e37d248f
SeifHecker2099 <102517370+SeifHossam17@users.noreply.github.com>
2023-12-24 20:41:50 +0200

    Update and rename lineage_gtaxllte.mk to evolution_gtaxllte.mk and change vendor dir from Lineage to Evolution.

--- evolution_gtaxllte.mk
+++ evolution_gtaxllte.mk
@@ -23,11 +23,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_tablet.mk)
+# Inherit some common Evolution X stuff.
+$(call inherit-product, vendor/evolution/config/common_full_tablet.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_gtaxllte
+PRODUCT_NAME := evolution_gtaxllte
 PRODUCT_DEVICE := gtaxllte
 PRODUCT_MODEL := SM-T585
 PRODUCT_BRAND := samsung
